3d3cc321a5157915657e7b69d917abbaee219816
Quinn Ramsay <qramsay7@gmail.com>
2023-11-26 16:09:51 -1000

    auto-populate text box cursor

--- src/components/SearchArea.tsx
+++ src/components/SearchArea.tsx
@@ -1,4 +1,4 @@
-import {FormEvent, useState } from 'react';
+import {FormEvent, useState, useRef, useEffect } from 'react';
 
 import { PossibleBook, getRecommendationService } from '../services/recommendations';
 
@@ -8,34 +8,42 @@ interface SearchAreaParams {
 }
 
 function SearchArea({setSearching, setRecommendations} : SearchAreaParams) {
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    searchInner();
-  }
-
-  const handleKeyDown = (e: any) => {
-    if (e.key === 'Enter' && !e.shiftKey) { // Check for Enter key and that Shift is not pressed
-        e.preventDefault(); // Prevents the default action (new line)
-        searchInner(); // Calls the submit function
-    }
-};
-
-function searchInner() {
-  setSearching(true);
-  setRecommendations([]);
-  getRecommendationService().getRecommendations(description).then(
-    (recommendations: PossibleBook[]) => {
-      setSearching(false);
-      setRecommendations(recommendations);
-    })
-    .catch((error: any) => {
-      console.error("Error fetching recommendations:", error);
-    });
-}
+    const searchTextRef = useRef<HTMLTextAreaElement>(null)
     const [description, setDescription] = useState("")
+    
+    useEffect(() => {
+        if(searchTextRef.current) {
+            searchTextRef.current.focus()
+        }
+    }, [])
+    const handleSubmit = (event: FormEvent) => {
+        event.preventDefault();
+        searchInner();
+    }
+
+    const handleKeyDown = (e: any) => {
+        if (e.key === 'Enter' && !e.shiftKey) { // Check for Enter key and that Shift is not pressed
+            e.preventDefault(); // Prevents the default action (new line)
+            searchInner(); // Calls the submit function
+        }
+    };
+
+    function searchInner() {
+    setSearching(true);
+    setRecommendations([]);
+    getRecommendationService().getRecommendations(description).then(
+        (recommendations: PossibleBook[]) => {
+        setSearching(false);
+        setRecommendations(recommendations);
+        })
+        .catch((error: any) => {
+        console.error("Error fetching recommendations:", error);
+        });
+    }
     return (<form className="search-area" onSubmit={handleSubmit}>
       <div className="search-box">
         <textarea
+          ref = {searchTextRef}
           className="description-text"
           placeholder="What are you looking for?"
           value={description}
